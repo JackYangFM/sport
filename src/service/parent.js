71398c4ca8b7c5432259b7d616ccd5dd36ec000e
Jack Yang <jackyangfm@pku.org.cn>
2019-07-28 20:34:48 +0800

    完成三个校区父母端页面，接口功能

---
+++ src/service/parent.js
@@ -0,0 +1,44 @@
+import request from '../utils/request'
+
+//课程安排初始化-五一校区
+export async function WuYiSelect(params, mock) {
+  return request('course_manage/WuYi_select', {
+    mock: mock ? mock : false,
+    body: {
+      method: 'GET',
+      data: params
+    }
+  })
+}
+
+//课程安排初始化-桐柏路校区
+export async function TongBaiLuSelect(params, mock) {
+  return request('course_manage/TongBaiLu_select', {
+    mock: mock ? mock : false,
+    body: {
+      method: 'GET',
+      data: params
+    }
+  })
+}
+
+//课程安排初始化-淮河路校区
+export async function HuaiHeLuSelect(params, mock) {
+  return request('course_manage/HuaiHeLu_select', {
+    mock: mock ? mock : false,
+    body: {
+      method: 'GET',
+      data: params
+    }
+  })
+}
+
+//家长选课提交
+export async function studentInsert(params, mock) {
+  return request('course_manage/student_insert', {
+    mock: mock ? mock : false,
+    body: {
+      data: params
+    }
+  })
+}
